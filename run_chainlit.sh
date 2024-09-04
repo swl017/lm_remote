3740f83f0f6f8fe786fed8e93a2e272f6100932c
Seungwook Lee <seungwook1024@gmail.com>
2024-09-04 12:03:49 +0900

    Adding run scripts

---
+++ run_chainlit.sh
@@ -0,0 +1,1 @@
+chainlit run src/app_chainlit.py -w --host 10.0.0.3 --port 5000
---
+++ run_proxy.sh
@@ -0,0 +1,1 @@
+sudo python3 src/forward_connections.py --src-ip 192.168.0.8  --src-port 8888 --dst-ip 10.0.0.3 --dst-port 5000 --log-level DEBUG
